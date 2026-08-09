4786db53408d8c663ffdd429f606094145dad013
Muhammad Adib Iktimal <adibiktimal765@gmail.com>
2026-08-09 23:16:27 +0700

    Ubah avatar default Ketua RT (Hamim) menjadi inisial bundar default

--- js/cms-data.js
+++ js/cms-data.js
@@ -106,7 +106,7 @@ const DEFAULT_DATA = {
     }
   ],
   pengurus: [
-    { name: "Hamim", role: "Ketua RT 03", phone: "+62 812-3456-7890", avatar: "img/about/IMG-20240716-WA0004.jpg" },
+    { name: "Hamim", role: "Ketua RT 03", phone: "+62 812-3456-7890", avatar: "" },
     { name: "Faqih Mubarok", role: "Ketua Karang Taruna", phone: "+62 813-9876-5432", avatar: "" },
     { name: "Aisyah", role: "Sekretaris RT", phone: "+62 815-1122-3344", avatar: "" },
     { name: "Ka Apin", role: "Bendahara RT", phone: "+62 878-5566-7788", avatar: "" }
